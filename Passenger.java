827b19484f2c28c2a4364fc4ea5836bc10b6b6b6
Danmcl01 <danielmclaren06@gmail.com>
2018-02-15 19:12:54 +0000

    Add files via upload
    Feel free to check these.
    HashCode still needed for Flight & Passenger.
    JUnit generateReportTest1 (Regex test) & generateReport in Flight not linked properly.

---
+++ Passenger.java
@@ -0,0 +1,65 @@
+public class Passenger {
+private String bookingRefCode;
+private String firstName;
+private String lastName;
+Flight flight;
+
+public Passenger(String bookingRefCode, String firstName, String lastName, Flight flight) {
+	this.setBookingRefCode(bookingRefCode);
+	this.setFirstName(firstName);
+	this.setLastName(lastName);
+	this.setFlight(flight);
+}
+
+	public String getBookingRefCode() {
+		return this.bookingRefCode;
+		}
+	
+	public void setBookingRefCode(String bookingRefCode) {
+		this.bookingRefCode = bookingRefCode;
+		}
+	
+	public String getFirstName() {
+		return this.firstName;
+		}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		}
+	
+	public String getLastName() {
+		return this.lastName;
+		}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+		}
+	
+	public Flight getFlight() {
+		return this.flight;
+		}
+	
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+		}
+	
+	
+	public boolean equals(Object obj)
+	{
+		return (obj instanceof Passenger) && (((Passenger)obj).getBookingRefCode().toUpperCase().equals(this.getBookingRefCode().toUpperCase()));
+	}
+
+	public int compareTo(Passenger passenger)
+	{
+		return this.getBookingRefCode().toUpperCase().compareTo(passenger.getBookingRefCode().toUpperCase());
+	}
+	
+
+
+}
+
+	
+	
+
+
+
