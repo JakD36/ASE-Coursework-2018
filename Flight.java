1238bf4a7aba3dc2c7c29950c8b1d0cf774af47b
agraham1988 <35735033+agraham1988@users.noreply.github.com>
2018-02-21 19:01:16 +0000

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,4 +1,3 @@
-
 public class Flight {
 	//flight description
 	private String flightCode;
@@ -19,9 +18,20 @@ public class Flight {
 	//total limits in the aircraft
 	private int passengerCapacity; 
 	
+	/**
+	 * Constructs a Flight object
+	 * @param flightCode the flight code
+	 * @param destination the destination
+	 * @param carrier the carrier
+	 * @param passengerCapacity the passenger capacity
+	 * @param maxVol the maximum volume of combined passenger luggage
+	 * @param maxWeight the maximum weight of combined passenger luggage
+	 * @param feeMultiplier used for calculating excess fees
+	 */
 	public Flight(String flightCode, String destination, String carrier,
 			int passengerCapacity, float maxVol, float maxWeight,float feeMultiplier) {
 
+		//assign the arguments to the class attributes
 		this.flightCode = flightCode;
 		this.destination = destination;
 		this.carrier = carrier;
@@ -36,34 +46,68 @@ public class Flight {
 		this.currentTotalFees = 0;
 	}
 	
+	/**
+	 * returns the flight code
+	 * @return the flight code
+	 */
 	public String getFlightCode() {
 		return flightCode;
 	}
 
+	/**
+	 * returns the destination
+	 * @return the destination
+	 */
 	public String getDestination() {
 		return destination;
 	}
 
+	/**
+	 * returns the max baggage volume
+	 * @return the max baggage volume
+	 */
 	public float getMaxBaggageVolume() {
 		return maxBaggageVolume;
 	}
 	
+	/**
+	 * returns the max baggage weight
+	 * @return the max baggage weight
+	 */
 	public float getMaxBaggageWeight() {
 		return maxBaggageWeight;
 	}
 
+	/**
+	 * returns the passenger capacity
+	 * @return the passenger capacity
+	 */
 	public int getPassengerCapacity() {
 		return passengerCapacity;
 	}
 
+	/**
+	 * returns the carrier
+	 * @return the carrier
+	 */
 	public String getCarrier() {
 		return carrier;
 	}
 
+	/**
+	 * returns the fee multiplier
+	 * @return the fee multiplier
+	 */
 	public float getFeeMultiplier(){
 		return feeMultiplier;
 	}
 	
+	/**
+	 * adds an item of baggage to the Flight
+	 * @param vol the volume of the baggage
+	 * @param weight the weight of the baggage
+	 * @param fee any excess baggage fees incurred
+	 */
 	public void addPassengerAndBaggage(float vol, float weight,float fee) {
 		//update weight and vol with this passenger's data
 		this.currentTotalBaggageWeight+=weight;
@@ -74,6 +118,10 @@ public class Flight {
 		this.currentTotalPassengers++;
 	}
 	
+	/**
+	 * Generates a summary of the Flight information as a String
+	 * @return the report String
+	 */
 	public String generateReport()
 	{
 		StringBuilder report = new StringBuilder();
@@ -86,20 +134,29 @@ public class Flight {
 		report.append(String.format("Flight code: %s\n",this.flightCode));
 		report.append(String.format("Number of Passengers: %d\n", this.currentTotalPassengers));
 		report.append(String.format("Total Baggage Weight: %.2f\n", this.currentTotalBaggageWeight));
-		report.append(String.format("Total Baggage Volume: %.2f\n", this.maxBaggageVolume));
+		report.append(String.format("Total Baggage Volume: %.2f\n", this.currentTotalBaggageVolume));
 		report.append(String.format("Total Excess Fees: %.2f\n", this.currentTotalFees));
 		report.append(String.format("Exceeded: %s\n",excess));
 		
 
 		return report.toString();
 	}
-	
+	/**
+	 * Compares if to Flights are equal
+	 * @param the object with which to compare
+	 * @return true if equal, false if unequal
+	 */
 	public boolean equals(Object obj)
 	{
 		//two flights are equal if their flight codes are equal  (case-insensitive)
 		return (obj instanceof Flight) && (((Flight)obj).getFlightCode().toUpperCase().equals(this.getFlightCode().toUpperCase()));
 	}
 	
+	/**
+	 * Compares a Flight's code with another Flight's code
+	 * @param flight the with which to compare
+	 * @return the result of the String comparison
+	 */
 	public int compareTo(Flight flight)
 	{
 		//comparisons is done w.r.t. the flight code (case-insensitive)
